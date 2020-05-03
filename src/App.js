615bb576928be96003c4f9adcab999fe353659e6
Christian Fernandez <cdferdez@gmail.com>
2020-05-02 19:10:13 -0500

    paginated fetch

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
-function isSearched(searchTerm) {
-    return function(item) {
-        return item.title.toLowerCase().includes(searchTerm.toLowerCase());    
-    }
-}
+// function isSearched(searchTerm) {
+//     return function(item) {
+//         return item.title.toLowerCase().includes(searchTerm.toLowerCase());    
+//     }
+// }
 
 class App extends Component {
     constructor(props) {
@@ -33,15 +36,28 @@ class App extends Component {
         this.fetchSearchTopStories(searchTerm);
     }
 
-    fetchSearchTopStories = (searchTerm) => {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    fetchSearchTopStories = (searchTerm, page=0) => {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
             .then(response => response.json())
             .then(result => this.setSearchTopStories(result))
             .catch(error => error);
     }
 
     setSearchTopStories(result) {
-        this.setState({result});
+        const { hits, page } = result;
+
+        const oldHits = page !== 0
+            ? this.state.result.hits
+            : [];
+
+        const updatedHits = [
+            ...oldHits,
+            ...hits
+        ];
+
+        this.setState({ 
+            result: { hits: updatedHits, page } 
+        });
     }
 
     // Dismisses item from the list
@@ -68,6 +84,7 @@ class App extends Component {
     render() {
         // ES6 Destructuring
         const {searchTerm, result} = this.state;
+        const page = (result && result.page) || 0;
 
         if (!result) { return null; }
         
@@ -82,12 +99,17 @@ class App extends Component {
                     </Search>                        
                 </div>
                 
+                {/* render only if the results object has been returned */}
                 { result &&
                     <Table 
                     list={result.hits}
                     onDismiss={this.onDismiss}
                     />
                 }
+
+                <div className="interactions">
+                    <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</Button>
+                </div>
 
             </div>
         );
