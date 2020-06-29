3e8b4f8ea571d21b4253c99c02aed7e4b9ae2655
Christian Fernandez <cdferdez@gmail.com>
2020-06-29 00:05:42 -0500

    small fixes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ class App extends Component {
         super(props);
         
         this.state = {
-            result: null,
+            results: null,
+            searchKey: '',
             searchTerm: DEFAULT_QUERY,
         };
 
@@ -33,6 +34,7 @@ class App extends Component {
 
     componentDidMount() {
         const { searchTerm } = this.state;
+        this.setState({ searchKey: searchTerm }); // the most recent term that was searched
         this.fetchSearchTopStories(searchTerm);
     }
 
@@ -45,9 +47,10 @@ class App extends Component {
 
     setSearchTopStories(result) {
         const { hits, page } = result;
+        const { searchKey, results } = this.state;
 
-        const oldHits = page !== 0
-            ? this.state.result.hits
+        const oldHits = results && results[searchKey]
+            ? results[searchKey].hits
             : [];
 
         const updatedHits = [
@@ -56,16 +59,26 @@ class App extends Component {
         ];
 
         this.setState({ 
-            result: { hits: updatedHits, page } 
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page}
+            }
         });
     }
 
     // Dismisses item from the list
     onDismiss(id) {
+        const { searchKey, results } = this.state;
+        const { hits, page } = results[searchKey];
+
         const isNotId = item => item.objectID !== id;
-        const updatedHits = this.state.result.hits.filter(isNotId);
+        const updatedHits = hits.filter(isNotId);
+
         this.setState({
-            result: {...this.state.result, hits: updatedHits}
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page }
+            }
         });
     }
 
@@ -77,16 +90,29 @@ class App extends Component {
     // used in event handler callback so has access to synthetic event object
     onSearchSubmit = (event) => {
         const { searchTerm } = this.state;
+        this.setState({ searchKey: searchTerm });
         this.fetchSearchTopStories(searchTerm);
         event.preventDefault();
     }
 
     render() {
         // ES6 Destructuring
-        const {searchTerm, result} = this.state;
-        const page = (result && result.page) || 0;
+        const { searchTerm, 
+                results,
+                searchKey } = this.state;
 
-        if (!result) { return null; }
+        const page = (
+            results &&
+            results[searchKey] &&
+            results[searchKey].page
+        ) || 0;
+
+        const list = (
+            results &&
+            results[searchKey] &&
+            results[searchKey].hits
+        ) || [];
+
         
         return (
             <div className="page">
@@ -100,15 +126,14 @@ class App extends Component {
                 </div>
                 
                 {/* render only if the results object has been returned */}
-                { result &&
-                    <Table 
-                    list={result.hits}
-                    onDismiss={this.onDismiss}
-                    />
-                }
+                
+                <Table 
+                list={list}
+                onDismiss={this.onDismiss}
+                />
 
                 <div className="interactions">
-                    <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</Button>
+                    <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>More</Button>
                 </div>
 
             </div>
